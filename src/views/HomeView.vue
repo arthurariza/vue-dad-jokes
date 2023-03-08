e11addeb51747f1ab00cc011b923fa1576b1c643
arthurariza <tucaariza@gmail.com>
2023-03-08 11:26:03 -0300

    Add tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <h1>Hello World</h1>
+    <h1 class="text-2xl">Hello World</h1>
   </main>
 </template>
